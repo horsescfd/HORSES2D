0571ee024f573841fbf53b06ceebe47edacfcb1a
Juan Manzanero <juan.manzanero@upm.es>
2016-12-30 13:49:00 +0100

    Tracking QuadDefinitions.f90

---
+++ Solver/src/Mesh/QuadDefinitions.f90
@@ -0,0 +1,15 @@
+module QuadMeshDefinitions
+   use SMConstants
+   implicit none  
+
+
+   integer, parameter         :: POINTS_PER_QUAD = 4
+   integer, parameter         :: POINTS_PER_EDGE = 2
+   integer, parameter         :: EDGES_PER_QUAD  = 4
+   integer, parameter         :: QUADS_PER_EDGE  = 2
+
+   integer, parameter         :: TOP = 3
+   integer ,parameter         :: BOTTOM = 4
+
+
+end module QuadMeshDefinitions
